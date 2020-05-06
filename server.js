4c5ef16ce6f1843f4457e0ca31924aae8f786452
Brian Fairbanks <brian.k.fairbanks@gmail.com>
2020-05-06 14:53:39 -0500

    corrected port typo to PORT

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const db = require("./models")
     Server Setup
 ================================================================*/
 
-const PORT = process.env.port || 8080;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
